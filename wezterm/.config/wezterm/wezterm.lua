7f8ebc69181dad56ee2de1a71528cf92b3473a01
leoa <leoa@apple.com>
2024-05-29 10:30:51 +0800

    wezterm:2

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,41 @@
 local wezterm = require("wezterm")
+local options = {
+	default_prog = {},
+	launch_menu = {},
+	keys = {},
+}
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	options.default_prog = { "zsh" }
+	options.launch_menu = {
+		{ label = " Top", args = { "top" } },
+		{ label = " Bash", args = { "bash" } },
+		{ label = " Zsh", args = { "zsh" } },
+	}
+	options.keys = {
+		{ key = "z", mods = "CMD", action = wezterm.action.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
+	}
+elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	options.default_prog = { "pwsh" }
+	options.launch_menu = {
+		{ label = " PowerShell v7", args = { "pwsh" } },
+		{ label = " PowerShell v1", args = { "powershell" } },
+		{ label = " Cmd", args = { "cmd" } },
+		{
+			label = " GitBash",
+			args = { "C:\\Program Files\\Git\\bin\\bash.exe" },
+		},
+	}
+	options.keys = {
+		{ key = "z", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
+	}
+end
+
 local config = {
 	font_size = 18,
 	font = wezterm.font("JetBrainsMonoNL Nerd Font Mono", { weight = "Regular" }),
 	color_scheme = "Catppuccin Mocha",
-
+	initial_cols = 100,
+	initial_rows = 28,
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
@@ -18,6 +50,9 @@ local config = {
 		top = 20,
 		bottom = 5,
 	},
+	keys = options.keys,
+	default_prog = options.default_prog,
+	launch_menu = options.launch_menu,
 }
 
 return config
