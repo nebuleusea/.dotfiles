f1b216ee9f8d68542cb3c79ed489e839decf2b47
leo <leo@arch.localdomain>
2024-06-20 22:04:06 +0800

    main:13

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,7 @@
 local wezterm = require("wezterm")
 local hostname = wezterm.hostname()
 
-local options = {
-	font_size = 0,
-	initial_cols = 0,
-	initial_rows = 0,
-	default_prog = {},
-	launch_menu = {},
-	keys = {},
-}
+local options = {}
 
 if hostname == "appledarwin" then
 	options.font_size = 15
@@ -32,6 +25,30 @@ elseif hostname == "jarmerarl" then
 	options.font_size = 13
 	options.initial_cols = 120
 	options.initial_rows = 28
+	options.colors = {
+		foreground = 'white',
+		background = 'black'
+	}
+	options.default_prog = { "nu", "-l" }
+	options.launch_menu = {
+		{ label = " Top", args = { "top" } },
+		{ label = " Bash", args = { "bash", "-l" } },
+		{ label = " Fish", args = { "fish", "-l" } },
+		{ label = " Nu", args = { "nu", "-l" } },
+	}
+	options.keys = {
+		{ key = "z", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
+		{
+			key = "F11",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	}
+elseif hostname == "arch" then
+	options.font_size = 28
+	options.initial_cols = 130
+	options.initial_rows = 28
+	options.window_background_opacity = 0.99
 	options.default_prog = { "nu", "-l" }
 	options.launch_menu = {
 		{ label = " Top", args = { "top" } },
@@ -76,16 +93,13 @@ local config = {
 	initial_cols = options.initial_cols,
 	initial_rows = options.initial_rows,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
-	colors = {
-		foreground = 'white',
-		background = 'black'
-	},
+	color = options.colors,
 	color_scheme = "Catppuccin Mocha",
 	use_fancy_tab_bar = false,
 	show_new_tab_button_in_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = false,
 	window_decorations = "RESIZE",
-	-- window_background_opacity = 0.99,
+	window_background_opacity = options.window_background_opacity,
 	text_background_opacity = 0.95,
 	-- macos_window_background_blur = 10,
 	adjust_window_size_when_changing_font_size = false,
