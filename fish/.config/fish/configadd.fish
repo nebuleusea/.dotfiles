5a988313000c87cf460474da2334da880bd70d29
leo <leo@arch.localdomain>
2024-06-30 23:17:57 +0800

    main:11

--- fish/.config/fish/configadd.fish
+++ fish/.config/fish/configadd.fish
@@ -1,5 +1,19 @@
 if status is-interactive
+    fastfetch
     # Commands to run in interactive sessions can go here
 end
 
 fzf --fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/leo/miniconda3/bin/conda
+    eval /home/leo/miniconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/home/leo/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/leo/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /home/leo/miniconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
